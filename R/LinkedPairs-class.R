bd796935d9731a17e9ba97fecd519b8d822a5b98
Cooley <npc19@fq-wifi-10-215-177-171.wireless.pitt.edu>
2020-02-18 12:28:51 -0500

    warnings and errors 001

--- R/LinkedPairs-class.R
+++ R/LinkedPairs-class.R
@@ -1,3 +1,25 @@
+`[.LinkedPairs` <- function(x, i, j, ...) {
+  ans <- NextMethod("[", x)
+  
+  if (missing(j))
+    return(ans)
+  
+  d <- dim(x)
+  I <- seq_len(d[1])
+  J <- seq_len(d[1])
+  d <- dimnames(x)
+  names(I) <- d[[1]]
+  names(J) <- d[[1]]
+  I <- I[i]
+  J <- J[j]
+  if (length(I) >= 2 &&
+      length(I)==length(J) &&
+      all(I==J) &&
+      !any(duplicated(I)))
+    class(ans) <- "LinkedPairs"
+  return(ans)
+}
+
 print.LinkedPairs <- function(x,
                             quote = FALSE,
                             right = TRUE,
